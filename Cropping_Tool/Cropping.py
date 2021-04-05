7a526044698176902980bbf1b2a6ff715f55e013
Anh Nhu <anhnhuvn2001@gmail.com>
2021-04-05 13:23:22 +0700

    Add Cropping Tool

--- Cropping_Tool/Cropping.py
+++ Cropping_Tool/Cropping.py
@@ -6,47 +6,53 @@ Deploy Demo
 """
 import cv2
 import os
+from PIL import Image
 
 from File_Browser import file_browser
-
+save_dir = "/Users/nhunh/SRMed/Dataset-Crop/Train-HR/"
 
 def click_and_crop(event, x, y, flags, param):
     # if the left mouse button was clicked, record the starting
-    # (x, y) coordinates and indicate that cropping is being
-    # performed
-    
-    # image = clone.copy()
+    # (x, y) coordinates and indicate that cropping is being performed
     if (event == cv2.EVENT_LBUTTONDOWN):
+        count = len(os.listdir(save_dir))
         """
         Patch retrieval
         """
-        patch = image[y-127:y+128, x-127:x+128]
+        patch = clone[y-128:y+128, x-128:x+128]
+
+        """
+        patch_PIL = cv2.cvtColor(patch, cv2.COLOR_BGR2RGB)
+        patch_save.save(os.path.join(save_dir, "train" + str(count)))
+
         cv2.imshow("Patch", patch)
-        patch_PIL = cv2.cvtColor(patch, cv2.COLOR_BGR2RGB) 
+        """
+        cv2.imwrite(os.path.join(save_dir, "train_" + str(count) + ".jpg"), patch)
         
-        cv2.rectangle(image, (x-127, y-127), (x+128, y+128), color = (0,255,0), thickness = 2)       
+        cv2.rectangle(image, (x-128, y-128), (x+128, y+128), color = (0,255,0), thickness = 2)       
         cv2.imshow("Image", image)
+
 #####################################################################################################################################################
 
-# Load the image, clone it, and setup the mouse callback function
-# img_dir = "/home/computer1/Downloads/Med/Sélection d_images D.Lamarque Partie I/Gastrite atrophique/Image_283416.png"
-img_dir = file_browser()
-image = cv2.imread(img_dir)
-clone = image.copy()
-cv2.namedWindow("Image")
-cv2.setMouseCallback("Image", click_and_crop)
+if __name__ == "__main__":
+    # Load the image, clone it, and setup the mouse callback function
+    # img_dir = "/home/computer1/Downloads/Med/Sélection d_images D.Lamarque Partie I/Gastrite atrophique/Image_283416.png"
+    img_dir = file_browser()
+    image = cv2.imread(img_dir)
+    clone = image.copy()
+    cv2.namedWindow("Image")
+    cv2.setMouseCallback("Image", click_and_crop)
 
-loop = True
-# keep looping until the Esc key is pressed
-while loop:
-    # display the image and wait for a keypress
-    cv2.imshow("Image", image)      
-    key = cv2.waitKey(0) & 0xFF
-    image = clone.copy()
-    # if the 'r' key is pressed, reset the cropping region
-    if key == ord("r") or key == 8:
-        image = clone.copy()
-    # if the Esc key is pressed, break from the loop
-    if key == 27:
-        loop = False
-        cv2.destroyAllWindows()
+    loop = True
+    # keep looping until the Esc key is pressed
+    while loop:
+        # display the image and wait for a keypress
+        cv2.imshow("Image", image)      
+        key = cv2.waitKey(0) & 0xFF
+        # if the 'r' key is pressed, reset the cropping region
+        if key == ord("r") or key == 8:
+            image = clone.copy()
+        # if the Esc key is pressed, break from the loop
+        if key == 27:
+            loop = False
+            cv2.destroyAllWindows()
--- Cropping_Tool/File_Browser.py
+++ Cropping_Tool/File_Browser.py
@@ -14,6 +14,6 @@ Allows user to select an image in the system and Returns the directory
 def file_browser():
     root = tkinter.Tk()
     root.withdraw()
-    file_dir = filedialog.askopenfile(mode = 'r', parent = root, initialdir = "./", title='Choose an Image', filetypes = (("ALL FILE", "*.*"), ("JPG", "*.jpg"), ("PNG", "*.png"), ("DICOM", "*.dicom")))
+    file_dir = filedialog.askopenfile(mode = 'r', parent = root, initialdir = "/Users/nhunh/SRMed/Dataset", title='Choose an Image', filetypes = (("ALL FILE", "*.*"), ("JPG", "*.jpg"), ("PNG", "*.png"), ("DICOM", "*.dicom")))
     
     return file_dir.name # directory to the file
